9d9e798014c0523b155801c485a621b9f1b65d5a
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-29 17:06:49 +0100

    Add initial UserManager logic and properties

--- Managers/UserManager.cs
+++ Managers/UserManager.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheSeer.Models;
 
 namespace TheSeer.Managers
 {
-    internal class UserManager
+    internal static class UserManager
     {
+        private static List<User> RegisteredUsers = new List<User>();
+
+        public static void CreateUser(string username, string password, string email, string nickname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DeleteUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void AuthenticateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UpdateUserDetails(string detailToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GetUserDetails(string username, string detailWanted)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
