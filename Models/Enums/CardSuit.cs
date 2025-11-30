f12ffeacb0c906b5cd41f7186b7620186c7c6537
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-30 16:10:33 +0100

    add CardSuit and ReadingType Enum, and User class

---
+++ Models/Enums/CardSuit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer2.Models.Enums
+{
+	enum CardSuit
+	{
+		Arcana,
+		Oracle,
+		Angel,
+		Rune
+	}
+}
---
+++ Models/Enums/ReadingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer2.Models.Enums
+{
+	enum ReadingType
+	{
+		OneCard,
+		TwoCard,
+		ThreeCard,
+		FourCard,
+	}
+}
