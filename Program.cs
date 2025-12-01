ee76ee10f2685e1519fef141905cd884dbc3ee3d
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-01 12:21:18 +0100

    Minor testing

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace TheSeer2
+﻿using TheSeer2.Models.Enums;
+using TheSeer2.Services;
+
+namespace TheSeer2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var tarotService = new TarotService();
+            Console.WriteLine("Cards loaded successfully!");
         }
     }
 }
+
